12840c27696accbb131f187f7f73061b95a91434
Fabio Frazao <fsfrazao@gmail.com>
2016-06-02 11:15:22 -0230

    Add calculate_GDD function and a description

--- GDD.py
+++ GDD.py
@@ -1,1 +1,17 @@
 
+def calculate_GDD(min_t,max_t,base_t):
+    """ Calculates Growing Degree Days (GDD).
+
+    Args:
+        min_t (float): The minimum temperature.
+        max_t (float): The maximum temperature.
+        base_t (float): The base temperature.
+
+    Returns:
+        float: The number of growing degree days.
+
+        This value is always >=0.
+
+    """
+
+    pass
