cd172ee547efe4a05265b385a368402400c75d7c
Fabio Frazao <fsfrazao@gmail.com>
2016-06-03 22:10:16 -0230

    Add read_data function

---
+++ plot_cummulative_GDD.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def  read_data(file_name):
+    """Read GDD data from file and return a list with
+        the cummulative equivalent.
+
+        Args:
+            file (string): a path to the csv file containing the GDD data.
+
+        Returns:
+            list (float): a list with the cummulative GDDs
+
+    """
+    data=pd.read_csv(file_name,sep=',')
+    gdds=data['gdd']
+
+    
