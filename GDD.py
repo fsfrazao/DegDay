5997d8d998fac0b900e996ec94d87919bc932e6d
Fabio Frazao <fsfrazao@gmail.com>
2016-06-07 14:16:42 -0230

    Move file handling to process_file function

--- GDD.py
+++ GDD.py
@@ -2,12 +2,21 @@ import argparse
 import pandas as pd
 
 parser = argparse.ArgumentParser(description='Calculate Growing Degree Days.')
-parser.add_argument('file', metavar='file', type=str,
-                    help='the file for containing daily min and max \
+parser.add_argument('path', metavar='path', type=str,
+                    help='the path for file or folder containing daily min and max \
                     temperatures')
 
+parser.add_argument('output', metavar='output', type=str,
+                    help='path to directory in which outputs will be saved')
+
+parser.add_argument('--folder',action='store_true',
+                    help='Interprets the given path as a directory and\
+                    calculate GDD for all files within.'
+
+
+
 args = parser.parse_args()
-output_name=args.file.split('.csv')[0]+'_GDD.csv'
+
 
 def calculate_GDD(min_t, max_t, base_t, max_threshold):
     """ Calculates Growing Degree Days (GDD).
@@ -17,7 +26,7 @@ def calculate_GDD(min_t, max_t, base_t, max_threshold):
         max_t (float): The maximum temperature.
         max_threshold (float): Maximum values for max_t
         base_t (float): The base temperature.
-        
+
 
     Returns:
         float: The number of growing degree days.
@@ -32,14 +41,16 @@ def calculate_GDD(min_t, max_t, base_t, max_threshold):
     return max(GDD,0)
 
 
-#Read the file passesd as argument into a pandas dataframe
-df = pd.read_csv(args.file,sep=',')
+
+def process_file(file_name,output_dir ):
+    output=output_dir+file_name('.csv')[0]+'_GDD.csv'
+    df = pd.read_csv(file_name,sep=',')
 
 
-gdds=df.apply(lambda row: calculate_GDD(min_t=row['min_t'],\
-                max_t=row['max_t'],base_t=10,max_threshold=30) ,axis=1)
+    gdds=df.apply(lambda row: calculate_GDD(min_t=row['min_t'],\
+                    max_t=row['max_t'],base_t=10,max_threshold=30) ,axis=1)
 
-df=pd.concat([df,gdds],axis=1)
-df.rename(columns={0:'gdd'},inplace=True)
+    df=pd.concat([df,gdds],axis=1)
+    df.rename(columns={0:'gdd'},inplace=True)
 
-df.to_csv(output_name,sep=',')
+    df.to_csv(output,sep=',')
