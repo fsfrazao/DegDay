0ac372d43ebb4f67ba56d230d099f75e97ea8259
djo504 <dayo.osunrinde@mun.ca>
2016-06-07 01:28:57 -0230

    Add url_data_cleaner function decription to urlcode.py

--- urlcode.py
+++ urlcode.py
@@ -11,8 +11,31 @@ def url_assembler(stationid,year):
     Returns:
         A complete url string that changes depending on the year and stationId args passed.
         For example: if stationid='5050'and year= 2013
-        url= "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?
-        format=csv&stationID="5050"&Year="2013"&timeframe=2&submit=Download+Data"
+        url= "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID="5050"&Year="2013"&timeframe=2&submit=Download+Data"
 
+    """
+    pass
+
+
+
+def data_cleaner(url,year,stationid):
+    
+    """Reads files directly from the url and cleans the data 
+   
+    Uses python pandas module to read directly from the url,clean the data
+    and,index the position and rename the columns.Output to a csvfile. 
+    Open the file and remove newline charsplit the string.
+    Then loop over the lines to write manipulated output to file
+    
+
+    Args:
+        url:  The string variable returned by the first function 
+        year: A string variable that changes ranging from 1894 to 2016
+        stationid : A string variable that changes depending on the user's preference
+        
+    Returns:
+        A completely automated downloaded cleaned csv file
+        with just the renamed needed columns of the given stationids and years
+        
     """
     pass
