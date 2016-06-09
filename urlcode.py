37a15f5074b4cee8e9f3b59480e7f251eaff630b
djo504 <dayo.osunrinde@mun.ca>
2016-06-09 20:18:49 -0230

    script level docstring fixed

--- urlcode.py
+++ urlcode.py
@@ -1,8 +1,32 @@
+
+   """ 
+ This program automates the GDD calulation download process by
+      assembling the url, cleans the data and
+      produce the data meant for GDD calculation
+     
+    Commandline Args:
+      
+        input_GDD.txt : The editable text file indicating stationid and year
+        we could get more stationid and year data by simply adding more to
+        stationid to line 0 and years to line 1 of this file
+
+        folder: The folder that saves the data of the years and stationid 
+        specified by the input_GDD.txt
+
+    
+    Example Usage: python3 urlcode.py input_GDD.txt folder
+
+   """
+
+
 import os
 import sys
 import pandas as pd
+
+
 def url_assembler(stationid,year):
-  
+
+
     """Assembles the url based on the specification on the input file
        Retrieves the url pertaining to the given arguments
 
@@ -11,6 +35,7 @@ def url_assembler(stationid,year):
         year: A string variable on the line 1 of input_GDD.txt file
         
     Returns:
+       
         None
     """
 
@@ -28,7 +53,7 @@ def data_cleaner(url,year,stationid):
     """Reads files directly from the url and cleans the data 
    
     Uses python pandas module to read directly from the url,clean the data
-    and,index the positio and rename the columns. 
+    and,index the position and rename the columns. 
     It works with the given lines in the input_Gdd.txt and output to a folder
     
 
@@ -38,20 +63,9 @@ def data_cleaner(url,year,stationid):
         stationid : A string variable that changes depending 
         on the entry on input_GDD.txt file
 
-    Commandline Args:
-        input_GDD.txt : The editable text file indicating stationid and year
-        we could get more stationid and year data by simply adding more to
-        stationid to line 0 and years to line 1 of this file
-        
-        folder: The folder that saves the data of the years and stationid 
-        specified by the input_GDD.txt
-
-    Example Usage: python3 urlcode.py input_GDD.txt folder
-
-    Returns:
-        None
     """
-    global output_folder
+
+  
     output_name=str(output_folder) + '/' + stationid + "_"+ year+".csv"
     data= pd.read_csv(url,sep=',',skiprows=25)#Skip the first 25rows of the url csv file
     columns= [1,2,3,5,7] # Index by position on the csv 
