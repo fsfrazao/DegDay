024eebb68c7d5a2f8cc1998648e1a5ab46e2f9ee
djo504 <dayo.osunrinde@mun.ca>
2016-06-11 00:43:05 -0230

    Add lambert canonical map and variables for plot

--- canada_GDDmap.py
+++ canada_GDDmap.py
@@ -2,3 +2,15 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import numpy as np
 #Ref:http://matplotlib.org/basemap/users/examples.html  
+
+#Variable names
+#llcrnrlon      longitude of lower left hand corner of the selected map domain.
+#llcrnrlat      latitude of lower left hand corner of the selected map domain.
+#urcrnrlon      longitude of upper right hand corner of the selected map domain.
+#urcrnrlat      latitude of upper right hand corner of the selected map domain.
+
+#Lambert conformal Conic map
+m = Basemap(llcrnrlon=-147.9927, llcrnrlat=45.49, urcrnrlon=-36.4459, urcrnrlat$
+              projection='lcc', resolution = 'h', area_thresh = 10000.0,
+lat_1=-5., lat_0=31.84256, lon_0=-53.)
+
