b1cc334bbca9747e54c5dd73389cd03be3bcbde3
Fabio Frazao <fsfrazao@gmail.com>
2016-06-13 15:13:50 -0230

    Change data_cleaner function so it only saves the date, min_tem and max_temp columns

--- urlcode.py
+++ urlcode.py
@@ -1,18 +1,18 @@
-""" 
+"""
  This program automates the GDD calulation download process by
       assembling the url, cleans the data and
       produce the data meant for GDD calculation
-     
+
     Commandline Args:
-      
+
         input_GDD.txt : The editable text file indicating stationid and year
         we could get more stationid and year data by simply adding more to
         stationid to line 0 and years to line 1 of this file
 
-        folder: The folder that saves the data of the years and stationid 
+        folder: The folder that saves the data of the years and stationid
         specified by the input_GDD.txt
 
-    
+
     Example Usage: python3 urlcode.py input_GDD.txt folder
  """
 
@@ -31,9 +31,9 @@ def url_assembler(stationid,year):
     Args:
         stationid:A string variable that changes depending on the input_GDD.txt$
         year: A string variable on the line 1 of input_GDD.txt file
-        
+
     Returns:
-       
+
         None
     """
 
@@ -47,37 +47,37 @@ os.popen("mkdir "+ str(output_folder)) #Create a directory for the output
 
 
 def data_cleaner(url,year,stationid):
-    
-    """Reads files directly from the url and cleans the data 
-   
+
+    """Reads files directly from the url and cleans the data
+
     Uses python pandas module to read directly from the url,clean the data
-    and,index the position and rename the columns. 
+    and,index the position and rename the columns.
     It works with the given lines in the input_Gdd.txt and output to a folder
-    
+
 
     Args:
-        url:  The string variable returned by the first function 
+        url:  The string variable returned by the first function
         year: A string variable on line 1 of the input_Gdd.txt file
-        stationid : A string variable that changes depending 
+        stationid : A string variable that changes depending
         on the entry on input_GDD.txt file
 
     """
 
-  
+
     output_name=str(output_folder) + '/' + stationid + "_"+ year+".csv"
     data= pd.read_csv(url,sep=',',skiprows=25)#Skip the first 25rows of the url csv file
-    columns= [1,2,3,5,7] # Index by position on the csv 
+    columns= [0,5,7] # Index by position on the csv
     clean_data=data[columns]
     clean_data.is_copy= False
     col_names=clean_data.columns
-    clean_data.rename(columns={col_names[0]:'year',col_names[1]:'month',col_names[2]:'day',\
-    col_names[3]:'max_temp' ,col_names[4]:'min_temp'},inplace=True)
+    clean_data.rename(columns={col_names[0]:'date'\
+    col_names[1]:'max_temp' ,col_names[2]:'min_temp'},inplace=True)
     clean_data.to_csv(output_name,sep=',',index=False)
 
 with open(input_file,'r')as input_file:
     lines=input_file.readlines()
-    lines=[line.rstrip('\n') for line in lines] 
-    lines=[tuple(line.split(',')) for line in lines] 
+    lines=[line.rstrip('\n') for line in lines]
+    lines=[tuple(line.split(',')) for line in lines]
 
 for line in lines:
     stationid=line[0]
