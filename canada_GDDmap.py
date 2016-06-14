7db1f2f62e697627d79b8e2b4a921febf6370ed7
djo504 <dayo.osunrinde@mun.ca>
2016-06-14 20:49:54 -0230

    Fixing readbility and clarity

--- canada_GDDmap.py
+++ canada_GDDmap.py
@@ -21,6 +21,28 @@ from matplotlib import cm as cm2
 
 #Ref:http://matplotlib.org/basemap/users/examples.html  
 
+# The effective GDD average for 5years
+EGDD = np.array([1310.1, 554.34, 1098.8, 1388.8, 318.82, 618.9,989.86])
+
+
+# Create map of canada using Lambert Conformal Conic projection
+m = Basemap(projection='lcc',
+            #With high resolution
+            resolution = 'h',
+            #And threshold 10000.0
+            area_thresh = 10000.0,
+            #Centered at
+            lat_0=30.83158,lon_0=-50.,
+            lat_1=-4.,
+            #longitude of lower left hand corner
+            llcrnrlon=-147.9927,
+            #latitude of lower left hand corner
+            llcrnrlat=45.49,
+            #Longitude of upper right hand corner
+            urcrnrlon=-36.4459,
+            #Latitude of upper left hand corner
+            urcrnrlat=72.8125 )
+
 location_data = {'latitude':[43.670495, 53.5444 , 49.406457, 45.500137, 52.139572, 47.570861], 
                 'longitude':[-79.400041,-113.4909 ,-123.151382,-73.563254 ,-106.646736, -52.707866]}
 df = pd.DataFrame(location_data,columns=['latitude', 'longitude'])
@@ -33,24 +55,6 @@ y_offsets = [5000, -25000, -25000 , -25000, 5000, 4000 ,-25000, 10000]
 for label, xpt, ypt,x_offset, y_offset in zip(labels, x, y,x_offsets,y_offsets):
     plt.text(xpt + x_offset, ypt + y_offset, label)
 
-
-# Create map of canada using Lambert Conformal Conic projection
-m = Basemap(projection='lcc',
-            #With high resolution
-            resolution = 'h', 
-            #And threshold 10000.0
-            area_thresh = 10000.0,
-            #Centered at 
-            lat_0=30.83158,lon_0=-50.,
-            lat_1=-4., 
-            #longitude of lower left hand corner
-            llcrnrlon=-147.9927, 
-            #latitude of lower left hand corner
-            llcrnrlat=45.49, 
-            #Longitude of upper right hand corner
-            urcrnrlon=-36.4459,
-            #Latitude of upper left hand corner
-            urcrnrlat=72.8125 ) 
 
 # Draw coastlines 
 m.drawcoastlines()
