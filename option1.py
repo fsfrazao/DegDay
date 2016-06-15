d69c7f32353db2d17314bfb77845ef21274d49b7
Dianlong Zhang(dz1834) <dz1834@mun.ca>
2016-06-15 02:45:44 -0230

    Update option1.py

--- option1.py
+++ option1.py
@@ -1,94 +1,65 @@
-from os.path import join, dirname
 import numpy as np
+import matplotlib.pyplot as pyplot
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as dt
+from pandas import Series, DataFrame
 import pandas as pd
+import csv
+import sys
+from pylab import *
+import io
+import os
+from glob import glob
 
-from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource, DataRange1d, Range1d, VBox, HBox, Select
-from bokeh.palettes import Blues4
-from bokeh.plotting import Figure
-from scipy.signal import savgol_filter
+fig,ax1=plt.subplots()
 
-STATISTICS = ['record_min_temp', 'actual_min_temp', 'average_min_temp', 'average_max_temp', 'actual_max_temp', 'record_max_temp']
-
-# Filter for smoothing data originates from http://stackoverflow.com/questions/20618804/how-to-smooth-a-curve-in-the-right-way
-def get_dataset(src, name, distribution):
-    df = src[src.airport == name].copy()
-    del df['airport']
-    df['date'] = pd.to_datetime(df.date)
-    df['left'] = df.date - pd.DateOffset(days=0.5)
-    df['right'] = df.date + pd.DateOffset(days=0.5)
-    df = df.set_index(['date'])
-    df.sort_index(inplace=True)
-    if distribution == 'Smooth':
-        window, order = 51, 3
-        for key in STATISTICS:
-            df[key] = savgol_filter(df[key], window, order)
-
-    return ColumnDataSource(data=df)
+def read_weather(file_name):
+    
+    data=pd.read_csv(file_name, usecols=(1,2,3,4),encoding='ISO-8859-1',delimiter =',') 
+    
+    data = data.dropna(how='any')
+    date = data['date']
+    max_temp = data['max_temp']
+    min_temp = data['min_temp']
+    gdd = data['gdd']
+    return data, max_temp, min_temp, gdd
 
 
-def make_plot(source, title):
-    plot = Figure(x_axis_type="datetime", plot_width=1000, tools="", toolbar_location=None)
-    plot.title = title
-    colors = Blues4[0:3]
+def plot_gdd(filename1, filename2, filename3):
+    """
+    This  function is to plotting gdd, the function has at most three data file to plot gdd graph.
+    """
+        
+    data_to_plot2 = [filename1,filename2,filename3]
+    data_to_plot = [x for x in data_to_plot2 if x is not None]
+    data, max_temp, min_temp, gdd = read_weather(filename1)
+    index = np.arange(0, 365) 
+    
+    
+    ax1.plot(index,gdd,'bs',label=filename1[5:-8]+" year")
+    for i in data_to_plot:
+        c=len(data_to_plot)
+        data2, max_temp2, min_temp2, gdd2 = read_weather(i)
+        new_gdd2=gdd2[0:366]
+        y = new_gdd2/c
+        grd = plt.grid(True)
+        ax1.set_xlabel('Month',fontsize=12)
+        plt.xlabel("Month", fontsize=12)
+        ax1.set_ylabel('Daily Accumulated based on Celsius',fontsize=15, color='b')
+        ax1.set_title('Daily Growing Degress Days',fontsize=20, color='k')   
+    ax1.plot(index,y,label="Average",color='red',linewidth=2.5, linestyle="-")
+    ax1.legend(loc=2,shadow=True)
+  
+    filename = os.path.join("/home/darren/Downloads/output", filename1[:-3]+"png")
+    plot_min_max = plt.savefig(filename)
 
-    plot.quad(top='record_max_temp', bottom='record_min_temp', left='left', right='right', color=colors[2], source=source, legend="Record")
-    plot.quad(top='average_max_temp', bottom='average_min_temp', left='left', right='right', color=colors[1], source=source, legend="Average")
-    plot.quad(top='actual_max_temp', bottom='actual_min_temp', left='left', right='right', color=colors[0], alpha=0.5, line_color="black", source=source, legend="Actual")
-
-    # fixed attributes
-    plot.border_fill_color = "whitesmoke"
-    plot.xaxis.axis_label = None
-    plot.yaxis.axis_label = "Temperature (F)"
-    plot.axis.major_label_text_font_size = "8pt"
-    plot.axis.axis_label_text_font_size = "8pt"
-    plot.axis.axis_label_text_font_style = "bold"
-    plot.x_range = DataRange1d(range_padding=0.0, bounds=None)
-    plot.grid.grid_line_alpha = 0.3
-    plot.grid[0].ticker.desired_num_ticks = 12
-
-    return plot
-
-
-# set up callbacks
-def update_plot(attrname, old, new):
-    city = city_select.value
-    plot.title = cities[city]['title']
-
-    src = get_dataset(df, cities[city]['airport'], distribution_select.value)
-    for key in STATISTICS + ['left', 'right']:
-        source.data.update(src.data)
-
-# set up initial data
-city = 'Austin'
-distribution = 'Discrete'
-
-cities = {
-    'Austin': {
-        'airport': 'AUS',
-        'title': 'Austin, TX',
-    },
-    'Boston': {
-        'airport': 'BOS',
-        'title': 'Boston, MA',
-    },
-    'Seattle': {
-        'airport': 'SEA',
-        'title': 'Seattle, WA',
-    }
-}
-
-city_select = Select(value=city, title='City', options=sorted(cities.keys()))
-distribution_select = Select(value=distribution, title='Distribution', options=['Discrete', 'Smooth'])
-
-df = pd.read_csv(join(dirname(__file__), 'data/2015_weather.csv'))
-source = get_dataset(df, cities[city]['airport'], distribution)
-plot = make_plot(source, cities[city]['title'])
-
-city_select.on_change('value', update_plot)
-distribution_select.on_change('value', update_plot)
-
-controls = VBox(city_select, distribution_select)
-
-# add to document
-curdoc().add_root(HBox(controls, plot))
+path=os.chdir('/home/darren/Downloads')
+csv_folder_file = glob('*.csv')
+n=size(csv_folder_file)
+if n==1:
+    gdd=plot_gdd(csv_folder_file[0],None,None)
+elif n==2:
+    gdd=plot_gdd(csv_folder_file[0],csv_folder_file[1],None)
+else:
+    gdd=plot_gdd(csv_folder_file[0],csv_folder_file[1],csv_folder_file[2])
